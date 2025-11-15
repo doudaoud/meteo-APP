36e7b47e826cc14cf4a2b5f75eddb63240b74577
doudaoud <boudaoudreda1@gmail.com>
2025-11-15 21:56:34 +0100

    cree un nouveau projet react

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className='body'>
+      <div>
+        <nav>
+          <h3 className='title-app'>
+            Weather App
+          </h3>
+          
+        </nav>
+      </div>
+    </div>
+  )
+}
+
+export default App
